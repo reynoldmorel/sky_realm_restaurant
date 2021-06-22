add9b17007853737fbcc056764a4be640dd3b94b
Reynold Morel <reynoldmorel@gmail.com>
2021-06-22 18:56:35 -0400

    Added Product service and controller

---
+++ lib/sky_realm_restaurant/services/in_memory_store/product.ex
@@ -0,0 +1,102 @@
+defmodule SkyRealmRestaurant.Services.InMemoryStore.ProductService do
+  alias SkyRealmRestaurant.Entities.Product
+  alias SkyRealmRestaurant.Utils.GeneralUtils
+  alias SkyRealmRestaurant.Utils.FileUtils
+  @products_file "in_memory_store/products.txt"
+
+  defp read_products_file(),
+    do: FileUtils.read_entities_from_file(@products_file, Product)
+
+  defp write_products_file_content(content),
+    do: FileUtils.write_entities_to_file(@products_file, content)
+
+  def find_by_id(id),
+    do:
+      {:ok,
+       Enum.find(read_products_file(), fn %Product{id: product_id} ->
+         product_id == id
+       end)}
+
+  def find_all(), do: {:ok, read_products_file()}
+
+  def create(new_product = %Product{}) do
+    {:ok, current_products} = read_products_file()
+    current_date_unix = DateTime.to_unix(DateTime.utc_now())
+
+    product_id = GeneralUtils.generate_id("#{length(current_products)}")
+
+    new_product = %Product{
+      new_product
+      | id: product_id,
+        created_at: current_date_unix,
+        updated_at: current_date_unix
+    }
+
+    updated_current_products = [new_product | current_products]
+
+    case write_products_file_content(updated_current_products) do
+      :ok -> {:ok, new_product}
+      error -> error
+    end
+  end
+
+  def update(id, updated_product = %Product{}) do
+    {:ok, current_products} = read_products_file()
+
+    existing_product =
+      Enum.find(current_products, fn %Product{id: product_id} ->
+        product_id == id
+      end)
+
+    current_date_unix = DateTime.to_unix(DateTime.utc_now())
+
+    updated_product = %Product{
+      existing_product
+      | parent_product_id:
+          Map.get(
+            updated_product,
+            :parent_product_id,
+            existing_product.parent_product_id
+          ),
+        serial: Map.get(updated_product, :serial, existing_product.serial),
+        name: Map.get(updated_product, :name, existing_product.name),
+        display_name: Map.get(updated_product, :display_name, existing_product.display_name),
+        price: Map.get(updated_product, :price, existing_product.price),
+        supported_measure_units:
+          Map.get(
+            updated_product,
+            :supported_measure_units,
+            existing_product.supported_measure_units
+          ),
+        updated_at: current_date_unix
+    }
+
+    product_update_mapper = fn product ->
+      case product.id == id do
+        true ->
+          updated_product
+
+        false ->
+          product
+      end
+    end
+
+    updated_current_products = current_products |> Enum.map(product_update_mapper)
+
+    case write_products_file_content(updated_current_products) do
+      :ok -> {:ok, updated_product}
+      error -> error
+    end
+  end
+
+  def delete(id) do
+    {:ok, current_products} = read_products_file()
+
+    updated_current_products = current_products |> Enum.filter(fn product -> product.id != id end)
+
+    case write_products_file_content(updated_current_products) do
+      :ok -> {:ok, true}
+      error -> error
+    end
+  end
+end
