bf0fb6d214d032f3083d437b321b2871f186424b
Reynold Morel <reynoldmorel@gmail.com>
2021-06-22 19:13:49 -0400

    Added Status Hisotry service and controller

---
+++ lib/sky_realm_restaurant/controllers/status_history.ex
@@ -0,0 +1,16 @@
+defmodule SkyRealmRestaurant.Controllers.StatusHistoryController do
+  alias SkyRealmRestaurant.Entities.StatusHistory
+  alias SkyRealmRestaurant.Services.InMemoryStore.StatusHistoryService
+
+  def find_by_id(id), do: StatusHistoryService.find_by_id(id)
+
+  def find_all(), do: StatusHistoryService.find_all()
+
+  def create(new_status_history = %StatusHistory{}),
+    do: StatusHistoryService.create(new_status_history)
+
+  def update(id, updated_status_history = %StatusHistory{}),
+    do: StatusHistoryService.update(id, updated_status_history)
+
+  def delete(id), do: StatusHistoryService.delete(id)
+end
---
+++ lib/sky_realm_restaurant/services/in_memory_store/status_history.ex
@@ -0,0 +1,102 @@
+defmodule SkyRealmRestaurant.Services.InMemoryStore.StatusHistoryService do
+  alias SkyRealmRestaurant.Entities.StatusHistory
+  alias SkyRealmRestaurant.Utils.GeneralUtils
+  alias SkyRealmRestaurant.Utils.FileUtils
+  @status_histories_file "in_memory_store/status_histories.txt"
+
+  defp read_status_histories_file(),
+    do: FileUtils.read_entities_from_file(@status_histories_file, StatusHistory)
+
+  defp write_status_histories_file_content(content),
+    do: FileUtils.write_entities_to_file(@status_histories_file, content)
+
+  def find_by_id(id),
+    do:
+      {:ok,
+       Enum.find(read_status_histories_file(), fn %StatusHistory{id: status_history_id} ->
+         status_history_id == id
+       end)}
+
+  def find_all(), do: {:ok, read_status_histories_file()}
+
+  def create(new_status_history = %StatusHistory{}) do
+    {:ok, current_status_histories} = read_status_histories_file()
+    current_date_unix = DateTime.to_unix(DateTime.utc_now())
+
+    status_history_id = GeneralUtils.generate_id("#{length(current_status_histories)}")
+
+    new_status_history = %StatusHistory{
+      new_status_history
+      | id: status_history_id,
+        created_at: current_date_unix,
+        updated_at: current_date_unix
+    }
+
+    updated_current_status_histories = [new_status_history | current_status_histories]
+
+    case write_status_histories_file_content(updated_current_status_histories) do
+      :ok -> {:ok, new_status_history}
+      error -> error
+    end
+  end
+
+  def update(id, updated_status_history = %StatusHistory{}) do
+    {:ok, current_status_histories} = read_status_histories_file()
+
+    existing_status_history =
+      Enum.find(current_status_histories, fn %StatusHistory{id: status_history_id} ->
+        status_history_id == id
+      end)
+
+    current_date_unix = DateTime.to_unix(DateTime.utc_now())
+
+    updated_status_history = %StatusHistory{
+      existing_status_history
+      | model_id: Map.get(updated_status_history, :model_id, existing_status_history.model_id),
+        model_type:
+          Map.get(updated_status_history, :model_type, existing_status_history.model_type),
+        from_status:
+          Map.get(updated_status_history, :from_status, existing_status_history.from_status),
+        to_status: Map.get(updated_status_history, :to_status, existing_status_history.to_status),
+        assigned_chef_id:
+          Map.get(
+            updated_status_history,
+            :assigned_chef_id,
+            existing_status_history.assigned_chef_id
+          ),
+        from_time: Map.get(updated_status_history, :from_time, existing_status_history.from_time),
+        to_time: Map.get(updated_status_history, :to_time, existing_status_history.to_time),
+        updated_at: current_date_unix
+    }
+
+    status_history_update_mapper = fn status_history ->
+      case status_history.id == id do
+        true ->
+          updated_status_history
+
+        false ->
+          status_history
+      end
+    end
+
+    updated_current_status_histories =
+      current_status_histories |> Enum.map(status_history_update_mapper)
+
+    case write_status_histories_file_content(updated_current_status_histories) do
+      :ok -> {:ok, updated_status_history}
+      error -> error
+    end
+  end
+
+  def delete(id) do
+    {:ok, current_status_histories} = read_status_histories_file()
+
+    updated_current_status_histories =
+      current_status_histories |> Enum.filter(fn status_history -> status_history.id != id end)
+
+    case write_status_histories_file_content(updated_current_status_histories) do
+      :ok -> {:ok, true}
+      error -> error
+    end
+  end
+end
