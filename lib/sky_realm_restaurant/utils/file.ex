2cc5714c42bcc132c307a81c599ef89dc348f1a4
Reynold Morel <reynoldmorel@gmail.com>
2021-06-23 21:52:48 -0400

    Added support for queue

--- lib/sky_realm_restaurant/utils/file.ex
+++ lib/sky_realm_restaurant/utils/file.ex
@@ -17,5 +17,20 @@ defmodule SkyRealmRestaurant.Utils.FileUtils do
     end
   end
 
+  def read_queue_from_file(path, queue_struc) do
+    case File.read(path) do
+      {:ok, file_content} ->
+        decoded_queue = Jason.decode!(file_content)
+        queue = struct(queue_struc, GeneralUtils.convert_map_to_keyword_list(decoded_queue))
+
+        {:ok, queue}
+
+      _ ->
+        {:ok, struct(queue_struc)}
+    end
+  end
+
   def write_entities_to_file(path, content), do: File.write(path, Jason.encode!(content))
+
+  def write_queue_to_file(path, queue), do: File.write(path, Jason.encode!(queue))
 end
